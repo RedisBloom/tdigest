4dfbababad640b18da6248310d58578119a0c24a
filipecosta90 <filipecosta.90@gmail.com>
2020-02-14 14:31:18 +0000

    [add] added td_centroids_weight_at and td_centroids_mean_at header ( mostly for testing )

--- src/tdigest.h
+++ src/tdigest.h
@@ -191,14 +191,44 @@ extern "C"
  */
   double td_max(td_histogram_t *h);
 
-  /**
- * Get maximum value from the histogram.
- *
- * @param h The histogram you want to free.
- */
-  const double* td_centroids_mean(td_histogram_t *h);
 
-  const double* td_centroids_weight(td_histogram_t *h);
+  /**
+ * Get the full centroids weight array for 'this' histogram.  
+ *
+ * @param h "This" pointer
+ * 
+ * @return The full centroids weight array.
+ */
+  const double *td_centroids_weight(td_histogram_t *h);
+
+  /**
+ * Get the full centroids mean array for 'this' histogram.  
+ *
+ * @param h "This" pointer
+ * 
+ * @return The full centroids mean array.
+ */
+  const double *td_centroids_mean(td_histogram_t *h);
+
+  /**
+ * Get the centroid weight for 'this' histogram and 'pos'.  
+ *
+ * @param h "This" pointer
+ * @param pos centroid position.
+ * 
+ * @return The centroid weight.
+ */
+  double td_centroids_weight_at(td_histogram_t *h, int pos);
+
+  /**
+ * Get the centroid mean for 'this' histogram and 'pos'.  
+ *
+ * @param h "This" pointer
+ * @param pos centroid position.
+ * 
+ * @return The centroid mean.
+ */
+  double td_centroids_mean_at(td_histogram_t *h, int pos);
 
 #ifdef __cplusplus
 }
